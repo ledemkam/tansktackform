e7ef311e18242fdc9be264f52b6d54ba11585eaa
ledemkam <ledemkam@gmail.com>
2025-05-04 21:54:07 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="w-full h-full bg-background flex flex-col items-center">
+       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"> TanStack Form</h1>
+    </div>
+  )
+}
